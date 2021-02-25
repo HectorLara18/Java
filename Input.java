3f55c4179cdfdcff451b8dd1a490343009285693
hector lara <hectorlaras.18@gmail.com>
2021-02-25 10:31:41 -0600

    se suben archivos

---
+++ Input.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Input {
+    static class Entrada {
+        public static void main(String[] args) {
+            System.out.println("Ingresa una entrada: ");
+            Scanner myVar = new Scanner(System.in);
+            System.out.println(myVar.nextLine());
+        }
+    }
+}
