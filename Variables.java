d531e4f1c94dba6093338379dd8395118cbb460b
Hector Lara <hectorlaras.18@gmail.com>
2021-02-24 23:45:35 -0600

    asdf

---
+++ Variables.java
@@ -0,0 +1,10 @@
+public class Variables {
+    public static void main(String[] args){
+        
+        double gradosCentigrados = 32.0;
+
+        System.out.println("Hola Mundo");
+        System.out.println(gradosCentigrados);
+
+    }
+}
