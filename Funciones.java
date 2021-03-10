6a4e00ca5bfb8810f175d6119b4859eee5bb3b6d
hlara <hectorlaras.18@gmail.com>
2021-03-10 09:22:36 -0600

    se agregan comentarios en comentario.txt

--- Funciones.java
+++ Funciones.java
@@ -4,9 +4,15 @@ public class Funciones {
         return lado1 * lado2;
     } 
 
+
+
     public static void main(String[] args){
         int x = 12;
         int y = 11;
+        byte i = 1;
+        byte j = 1;
+        byte k = i + j;
+
         double cuadrado1 = areaCuadrado(x, y);
         System.out.println(cuadrado1);
     }
